496b3611731bee68fa2660eee620fb8f4125b64b
mitsuru <sanglam2806@gmail.com>
2025-02-04 21:00:10 +0900

    update exercice

--- test/comptimeTest.zig
+++ test/comptimeTest.zig
@@ -1,11 +1,19 @@
 const print = @import("std").debug.print;
 
-pub fn main () void {
+const size = 5;
+const test_slice = [5]u8{1,2,3,4,5};
 
-    const c =  multiply(4,5);
-    print("4 X 5 = {d}", .{c});
+fn getValueOfIndex(comptime i: usize) u8 {
+    comptime isAlive( i < size);
+    return test_slice[i];
 }
 
-fn multiply(a: u8, b:u8) u64 {
-    return a*b;
+fn isAlive (ok: bool) void {
+    if (!ok) unreachable;
+}
+
+pub fn main () void {
+    const x = getValueOfIndex(size - 1);
+
+    print("the last value of slice is {d} ", .{x});
 }
